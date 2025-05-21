309846c526ce65b4a2f8a99599be1e261d8d33af
LeeJaeHyunJohn <jaehyun1030@naver.com>
2025-05-22 03:47:57 +0900

    Initial commit: added headers and base structure

---
+++ Desktop/고려대학교4-1/운영체제/OS_termproject/CPU_Scheduiling_simulator.c
@@ -0,0 +1,34 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+int Rank_Waiting_Time[6] = { 0, 1, 2, 3, 4, 5 };
+int Rank_Turnaround_Time[6] = { 0, 1, 2, 3, 4, 5 };
+int Rank_CPU_Utilization[6] = { 0, 1, 2, 3, 4, 5 };
+float Average_Waiting_Time[6], Average_Turnaround_Time[6], CPU_Utilization[6];
+int left_IO[6][5];
+
+typedef struct {
+	int Process_ID;
+	int CPU_Burst_Time;
+	int IO_Burst_Time;
+	int Arrival_Time;
+	int Priority;
+	int IO_Burst_Timing;
+}Process;
+
+typedef struct {
+	int* ID;
+	int cnt;
+}QUEUE;
+
+QUEUE* Create_Queue(void) {
+	QUEUE* buf = (QUEUE*)malloc(sizeof(QUEUE));
+	buf->ID = (int*)malloc(sizeof(int) * 1000);
+	buf->cnt = 0;
+	return buf;
+}
